f792540d0126b8b0c5870264603eedf6ccf6744b
Phil Spitzer <spitzerpl@outlook.com>
2026-02-20 16:44:54 -0600

    Update website

--- devotionals/devo-52-05-01.js
+++ devotionals/devo-52-05-01.js
@@ -1,21 +1,24 @@
-// Feasting on FAithHOpeLOve with 1 Thessalonians 5:1\u201311
+// Feasting on FAithHOpeLOve with 1 Thessalonians 5:1\u201315
 DEVOTIONALS.push({
   id: 9,
   published: "2025-02-15",
-  reviewed: "2025-02-15",
-  title: "Feasting on FAithHOpeLOve with 1 Thessalonians 5:1\u201311",
-  reference: "1 Thessalonians 5:1\u201311",
+  reviewed: "2025-02-20",
+  status: "published",
+  title: "Feasting on FAithHOpeLOve with 1 Thessalonians 5:1\u201315",
+  reference: "1 Thessalonians 5:1\u201315",
   theme: "all",
   tags: ["faith", "hope", "love"],
   scripture: "\u201CBut since we belong to the day, let us be sober, having put on the breastplate of faith and love, and for a helmet the hope of salvation.\u201D (1 Thessalonians 5:8, ESV)",
-  preview: "We are children of the day living in a land of darkness. Those who walk in unwelcoming territory must dress accordingly.",
-  body: `<p>Those subject for future destruction falsely said \u201CThere is peace and security\u201D (<b>3</b>). We too believe in peace and security, but it is only in a salvation obtained through our Lord Jesus Christ (<b>9</b>). Peace and security outside of Christ is false and empty. When judgement comes, it will be revealed as worthless (<b>3</b>).</p>
-<p>Until Christ brings such peace and security, we will remain pilgrims in a foreign land (1 Pet 2:11, Heb 11:13, Phil 3:20). We are children of the day living in a land of darkness (<b>4\u20136</b>). Those who walk in unwelcoming territory must maintain awareness of that fact (<b>6</b>) and must dress accordingly (<b>8</b>). This \u201Cspiritual apparel\u201D is where we find Paul turning to FAHOLO.</p>
+  preview: "What is true peace and security? Merely claiming it doesn\u2019t make it so. Its pure form is only found in a salvation obtained through our Lord Jesus Christ.",
+  body: `<p>What is true peace and security? Merely claiming it doesn\u2019t make it so (<b>3</b>). Its pure form is only found in a salvation obtained through our Lord Jesus Christ (<b>9</b>). Outside of Christ, attempts to obtain it are false and empty. When judgment comes, it will be revealed as worthless (<b>3</b>).</p>
+<p>That false sense feels comforting in the moment, but leads to darkness (<b>4</b>) and pain (<b>3b</b>). Only by God\u2019s grace have greater things been revealed to us (<b>1\u20132</b>). Only by His cross do we live in Him (<b>9\u201310</b>) and have awareness of what is ahead (<b>4</b>).</p>
+<p>Until Christ brings his peace and security, we will remain pilgrims in a foreign land (1 Pet 2:11, Heb 11:13, Phil 3:20). We are children of the day living in a land of darkness (<b>4\u20136</b>). Those who walk in unwelcoming territory must maintain awareness of that fact (<b>6</b>) and must dress accordingly (<b>8</b>). This \u201Cspiritual apparel\u201D is where we find Paul turning to FAHOLO.</p>
+<p>Soldiers are not solo and neither are we. We bear our armor and stand together (<b>11</b>), serve together (<b>12\u201314</b>), and represent God\u2019s goodness to all (<b>15</b>).</p>
 <h3 class="faith-heading">Faith</h3>
-<p>Here in vs. 8 faith and love describe the same piece of armor. They are frequent companions (Eph 3:17; 6:23, Col 1:7\u20138, 1 Thess 3:6, 1 Tim 1:5, 14, 2:15, 2 Tim 1:13 plus others). Without love, faith is nothing more than an intellectual exercise (1 Cor 13:2). Without faith, love is easily misdirected (1 Tim 6:10).</p>
+<p>Here in vs. 8 faith and love describe the same piece of armor. They are frequent companions (Luke 7:41\u201350; 1 Thess 3:6, Jud 20\u201321 among others). Without love, faith is nothing more than an intellectual exercise (1 Cor 13:2). Without faith, something as benign as money can undermine success pursuing FAHOLO (1 Tim 6:10\u201312).</p>
 <p>A solid faith ensures our propensity for self-indulgence does not skew us towards an ungodly love of the world (1 John 2:15). It centers us on and directs our life\u2019s focus toward Christ. <em>Is your life guarded by a persistent faith?</em></p>
 <h3 class="love-heading">Love</h3>
-<p>Godly love is so easily counterfeited. Rather than being the ultimate fruit of righteousness (Hos. 10:12), we slap it on anything we wish to justify. \u201CIf we call it love, it must be right\u201D seems to be the mantra of the day. However the love we are called to is a defined love. It is a love that echoes what our faith has discovered \u2013 that God first loved us (1 Jn 4:19). <em>Do the things that you treasure reflect the love you discovered by faith?</em></p>
+<p>Godly love is so easily counterfeited. Rather than being the fruit of righteousness (Hos. 10:12), we slap \u201Clove\u201D on anything we wish to justify. \u201CIf we call it love, it must be right\u201D seems to be the mantra of the day. However, the love we are called to is a defined love. It is a love that echoes what our faith has discovered \u2013 that God first loved us (1 Jn 4:19). <em>Do the things that you treasure reflect the love you discovered by faith?</em></p>
 <h3 class="hope-heading">Hope</h3>
-<p>Ours is not a shallow hope. We do not hope in empty things like money (1 Tim 6:17), power (Matt 20:25\u201328), or pleasure (Eccl 2:1). These things will meet sudden destruction (<b>3</b>) and wrath (<b>9</b>). Our hope is in our salvation; a salvation through our Lord Jesus Christ who alone is our \u201Chope of Glory.\u201D (Col 1:27). This is where our mind must constantly be, which makes the helmet of \u201Cthe hope of salvation\u201D a fitting piece of spiritual armor (<b>8</b>). <em>Is your Christian hope consistently on your mind?</em></p>`
+<p>Ours is not a shallow hope. We do not hope in empty things like money (1 Tim 6:17), power (Psalm 20:7), or pleasure (Eccl 2:1). These things will meet sudden destruction (<b>3</b>) and wrath (<b>9</b>). Our hope is in our salvation; a salvation through our Lord Jesus Christ who alone is our \u201Chope of glory\u201D (Col 1:27). This is where our mind must constantly be, which makes the helmet of \u201Cthe hope of salvation\u201D a fitting piece of spiritual armor (<b>8</b>). <em>Is your Christian hope consistently on your mind?</em></p>`
 });
